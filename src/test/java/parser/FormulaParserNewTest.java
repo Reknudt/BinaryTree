243f6ff44e8927b1433837093a92591509cd5fa9
Reknudt <pochta.navsegda@mail.ru>
2025-01-27 15:39:13 +0300

    Parser work, but still bugs

---
+++ src/test/java/parser/FormulaParserNewTest.java
@@ -0,0 +1,387 @@
+package parser;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+import static java.math.BigDecimal.ROUND_HALF_UP;
+import static parser.FormulaParserNewTest.FunctionType.functionsWithArgs;
+
+public class FormulaParserNewTest {
+
+    public enum FunctionType {
+        CNTOP {
+            @Override
+            public BigDecimal apply() {
+                return new BigDecimal("-0.11");
+            }
+        },
+        SUMOP {
+            @Override
+            public BigDecimal apply() {
+                return new BigDecimal("2.555");
+            }
+        },
+        RESERV {
+            @Override
+            public BigDecimal apply() {
+                return new BigDecimal("0.888888");
+            }
+        },
+        ROUNDCOST {
+            @Override
+            public BigDecimal apply(BigDecimal a) {
+                BigDecimal divisor = new BigDecimal(4);
+                return a.divide(divisor, 4, BigDecimal.ROUND_HALF_EVEN);
+            }
+        },
+        ROUNDOPERSUM {
+            @Override
+            public BigDecimal apply(BigDecimal a) {
+                return a.setScale(2, ROUND_HALF_UP );
+            }
+        };
+
+        public BigDecimal apply(BigDecimal a) {
+            return a;
+        }
+
+        public BigDecimal apply() {
+            return null;
+        }
+
+        static final String[] functionsWithArgs = {"ROUNDCOST", "ROUNDOPERSUM"};          //
+    }
+
+    public static String validate(String formula) {       //
+
+        formula = formula.replace(" ", "");
+        if (formula.isEmpty()) throw new IllegalArgumentException("String is empty");
+        formula = formula.toUpperCase();
+        formula = functionArgumentCheck(formula);
+        formula = emptyBracketsReplacer(formula);
+        formula = addBrackets(formula);
+
+        startsWithCheck(formula);
+        endsWithCheck(formula);
+        isFormulaValid(formula);
+        hasMultipleOperators(formula);
+        isBracketWithNoOperator(formula);
+        return formula;
+    }
+
+    public static String functionArgumentCheck(String formula) {
+        List<String> tokens = tokenize(formula);
+
+        for (int i = 0; i < tokens.size(); i++) {
+            String token = tokens.get(i);
+            if (i > 0 && i < tokens.size()-1 && token.equals("(") && tokens.get(i+1).equals(")")
+                && Arrays.stream(functionsWithArgs).toList().contains(tokens.get(i-1).replace("-",""))) {
+                throw new IllegalArgumentException("Functions with arguments such as " + Arrays.stream(functionsWithArgs).toList()
+                 + " must have argument");
+            }
+        }
+        return formula;
+    }
+
+    public static String emptyBracketsReplacer(String formula) {
+        formula = formula.replace("()", "");
+        if (formula.contains("()")) return emptyBracketsReplacer(formula);
+        return formula;
+    }
+
+    public static String addBrackets(String formula) {
+        if (formula.contains("(") || formula.contains(")")) {
+            int obCount = 0;
+            int cbCount = 0;
+
+            for(char c : formula.toCharArray()) {
+                if (c == '(') obCount++;
+                if (c == ')') cbCount++;
+            }
+            if (cbCount < obCount) {
+                while(cbCount < obCount) {
+                    formula = formula.concat(")");
+                    cbCount++;
+                }
+            } else if (cbCount > obCount) {
+                throw new IllegalArgumentException("Too much close brackets found");
+            }
+        }
+        return formula;
+    }
+
+    public static void startsWithCheck(String formula) {
+        if (formula.startsWith("*") || formula.startsWith("/")) {
+            throw new IllegalArgumentException("Formula can't start with '*' or '/'. " +
+                    " Maybe you meant " + formula.substring(1));
+        }
+    }
+
+    public static void endsWithCheck(String formula) {
+        String endStr = formula.substring(formula.length() - 1);
+        if (isOperator(endStr) || endStr.equals("(")) {
+            throw new IllegalArgumentException("Formula can't end with operators like '+' or '(' : " + endStr);
+        }
+    }
+
+    public static void isFormulaValid(String formula) {
+        List<String> tokens = tokenize(formula);
+
+        for (int i = 0; i < tokens.size(); i++) {
+            String token = tokens.get(i);
+            if (!isOperator(token) && !isNumber(token) && !isFunction(token)
+                && !token.equals("(") && !token.equals(")")) {
+                throw new IllegalArgumentException("Illegal operation entered: " + token);
+            }
+            if (Arrays.stream(functionsWithArgs).toList().contains(tokens.get(i).replace("-",""))
+                    && i == tokens.size()-1) {
+                throw new IllegalArgumentException("This function needs bracket '(' : " + token);
+            }
+            if (Arrays.stream(functionsWithArgs).toList().contains(tokens.get(i).replace("-",""))
+                    && i < tokens.size() && !tokens.get(i+1).equals("(")) {
+                throw new IllegalArgumentException("This function needs bracket '(' : " + token);
+            }
+//             && !tokens.get(i+1).equals("(")
+        }
+    }
+
+    public static void hasMultipleOperators(String formula) {
+        List<String> tokens = tokenize(formula);
+
+        for (int i = 0; i < tokens.size(); i++) {
+            String token = tokens.get(i);
+            if (isOperator(token) && (isOperator(tokens.get(i+1)) || tokens.get(i+1).contains("-"))) {
+                throw new IllegalArgumentException("Too many operators entered: " + token + tokens.get(i+1));
+            }
+        }
+    }
+
+    public static void isBracketWithNoOperator(String formula) {
+        List<String> tokens = tokenize(formula);
+
+        for (int i = 0; i < tokens.size(); i++) {
+            String token = tokens.get(i);
+
+            if ((token.equals("(") && i != 0)
+                    && (!isOperator(tokens.get(i - 1))
+                        && !tokens.get(i-1).equals("(")
+                        && !Arrays.stream(functionsWithArgs).toList().contains(tokens.get(i - 1).replace("-","")))) {
+                throw new IllegalArgumentException("Illegal operator placement " + tokens.get(i - 1) + token);
+            }
+//            if (token.equals("(") && i < tokens.size()-1) {
+//                if (isOperator(tokens.get(i+1))
+//                        && !Arrays.stream(functionsWithArgs).toList().contains(tokens.get(i - 1).replace("-",""))) {
+//                    throw new IllegalArgumentException("Illegal operator placement " + token + tokens.get(i + 1));
+//                }
+//            }
+
+            if (token.equals(")")) {
+                if (i == 0) {
+                    throw new IllegalArgumentException("Illegal ')' placement");
+                }
+                if (isOperator(tokens.get(i - 1))) {
+                    throw new IllegalArgumentException("Illegal operator placement: " + tokens.get(i - 1) + token);
+                }
+                if (i < tokens.size() - 1 && (!isOperator(tokens.get(i + 1)) && !tokens.get(i + 1).equals(")"))) {
+                    throw new IllegalArgumentException("Illegal  placement: " + token + tokens.get(i + 1));
+                }
+            }
+        }
+    }
+
+    public static BigDecimal calculate(String expression) {
+        List<String> tokens = tokenize(expression);
+        Queue<String> postfix = infixToPostfix(tokens);
+        return calculatePostfix(postfix);
+    }
+
+    private static List<String> tokenize(String expression) {
+        List<String> tokens = new ArrayList<>();
+        StringBuilder buffer = new StringBuilder();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+
+            if (Character.isDigit(ch) || (ch == '.' && !buffer.isEmpty())) {
+                buffer.append(ch);
+
+            } else if (Character.isLetter(ch)) {
+                buffer.append(ch);
+
+            } else {
+                if (!buffer.isEmpty()) {
+                    tokens.add(buffer.toString());
+                    buffer.setLength(0);
+                }
+                if (ch == '-'
+                        && (i == 0 || expression.charAt(i - 1) == '('
+                        || isOperator(String.valueOf(expression.charAt(i - 1))))) {
+
+                    buffer.append(ch);
+                } else {
+                    tokens.add(String.valueOf(ch));
+                }
+            }
+        }
+
+        if (!buffer.isEmpty()) {
+            tokens.add(buffer.toString());
+        }
+        return tokens;
+    }
+
+    private static Queue<String> infixToPostfix(List<String> tokens) {
+        Queue<String> output = new LinkedList<>();
+        Deque<String> operators = new ArrayDeque<>();
+        Map<String, Integer> precedence = Map.of(
+                "+", 1, "-", 1,
+                "*", 2, "/", 2
+        );
+
+        for (String token : tokens) {
+            if (isNumber(token)) {
+                output.add(token);
+            } else if (!token.contains("-") && isFunction(token)
+                    && !Arrays.stream(functionsWithArgs).toList().contains(token)) {
+                output.add(token);
+            } else if (isFunction(token)) {
+                operators.push(token);
+            } else if (token.equals("(")) {
+                operators.push(token);
+            } else if (token.equals(")")) {
+                while (!operators.isEmpty() && !operators.peek().equals("(")) {
+                    output.add(operators.pop());
+                }
+                if (!operators.isEmpty() && operators.peek().equals("(")) {
+                    operators.pop();
+                    if (!operators.isEmpty() && isFunction(operators.peek())) {
+                        output.add(operators.pop());
+                    }
+                }
+            } else if (isOperator(token)) {
+                while (!operators.isEmpty()
+                        && precedence.getOrDefault(operators.peek(), 0) >= precedence.get(token)) {
+                    output.add(operators.pop());
+                }
+                operators.push(token);
+            }
+            System.out.println("operators " + operators + " token " + token + " output " + output);
+        }
+
+        while (!operators.isEmpty()) {
+            output.add(operators.pop());
+        }
+
+        return output;
+    }
+
+    private static BigDecimal calculatePostfix(Queue<String> postfix) {
+        Deque<BigDecimal> stack = new ArrayDeque<>();
+
+        while (!postfix.isEmpty()) {
+            String token = postfix.poll();
+
+            System.out.println("stack: " + stack + " postfix: " + postfix + " token: " + token);
+
+            if (isNumber(token)) {
+                stack.push(new BigDecimal(token));
+            } else if (isOperator(token)) {
+
+                BigDecimal b = stack.pop();
+
+                if ((token.equals("-") || token.equals("+")) && stack.isEmpty()) {      //if exp start with + or -
+                    stack.push(applyOperator(BigDecimal.valueOf(0), b, token));
+
+                } else {
+                    BigDecimal a = stack.pop();
+                    stack.push(applyOperator(a, b, token));
+                }
+            } else if (isFunction(token)) {
+                String noSubtractToken = token.replace("-", "");
+
+                FunctionType function = FunctionType.valueOf(noSubtractToken);
+                BigDecimal result;
+
+                if (!Arrays.stream(functionsWithArgs).toList().contains(noSubtractToken)) {
+                    result = function.apply();
+                } else {
+                    BigDecimal arg = stack.pop();
+                    result = function.apply(arg);
+                }
+
+                if (token.startsWith("-")) {
+                    result = result.negate();
+                }
+
+                stack.push(result);
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private static BigDecimal applyOperator(BigDecimal a, BigDecimal b, String operator) {
+        switch (operator) {
+            case "+":
+                return a.add(b);
+            case "-":
+                return a.subtract(b);
+            case "*":
+                return a.multiply(b);
+            case "/": {
+                try {
+                    return a.divide(b, 4, BigDecimal.ROUND_HALF_EVEN);
+                } catch (ArithmeticException e) {
+                    throw new ArithmeticException("Division by zero found");
+                }
+            }
+            default:
+                throw new IllegalArgumentException("Unknown operator: " + operator);
+        }
+    }
+
+    private static boolean isNumber(String token) {
+        try {
+            new BigDecimal(token);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean isOperator(String token) {
+        return "+-*/".contains(token);
+    }
+
+    private static boolean isFunction(String token) {
+        String baseToken = token.replace("-", "");
+        try {
+            FunctionType.valueOf(baseToken);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    @Test
+    void test1() {
+        String f1 = "2/roundopersum(-(-2)";
+
+//        String f1 = "33/(88*roundopersum(-(12-(-cntop)+7)*(1/sumop)-5))";
+//        String f1 = "2 * roundopersum(-(12-(-cntop)+7)*(1/sumop)-5)";
+//        String f1 = "ROUNDCOST(13.2342341341234";
+        f1 = validate(f1);
+        System.out.println(f1);
+        BigDecimal result = calculate(f1);
+        System.out.println(result);
+    }
+}
--- src/test/java/parser/ParserCasesTest.java
+++ src/test/java/parser/ParserCasesTest.java
@@ -3,47 +3,66 @@ package parser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserCasesTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"CNTOP() + 2.34234", "32.121212  +  CNTOP  ", "cntop + 23.234235", "ROUNDCOST(3 - 23)"})
+    @ValueSource(strings = {"33/(88-(12-(-cntop)+7)*(1/sumop)-5)"})//"CNTOP() + 2.34234()", "  32.121212  +  CNTOP  ", "cntop + 23.234235", "-2+3",
+//            "2*(3-ROUNDCOST(3 - 23", "--2+3", "2-3+", "2-3/", "2*-23.234)", "*3-2", "(CNTOP-2)CNTOP",
+//            "CNTOP(2-2)", "2.234.234-2", "---CNTOP", "(1-2)(1+2)", "SUB-CNTOP",
+//            "CNTOP(1-1)", "(1-1)CNTOP", "/3+2", "(/2+3)","(234-)", "    ",
+//            "28*(23.324234-cntop- Roundopersum (-234.345345345+(34.23/ ( -1)) /2)",
+//            "-  roundcost(-2.234234234)/cntop+2.234-(-reserv.*(cntop())", "-roundopersum()+2.234",
+//            "((22-2*3.343)+2334.234)-cntop", "(())*2.234", "111.111(((())))"})
     void validateTest(String formula) {
-        String vf = FormulaParserTest.validate(formula);
+        FormulaParserNewTest formulaParser = new FormulaParserNewTest();
+        String vf = formulaParser.validate(formula);
         System.out.println(vf);
     }
 
     @Test
     void calculateTest() {
+        BigDecimal a1 = new BigDecimal("533333280803.30855853353085");
+        BigDecimal a2 = new BigDecimal("-13.23");
 
-        Assertions.assertAll(
-                () -> assertEquals(BigDecimal.valueOf(2.11), FormulaParserTest.calculate("2.22 + CNTOP")),
-                () -> assertEquals(BigDecimal.valueOf(-4), FormulaParserTest.calculate("- 6 + 2")),
-                () -> assertEquals(BigDecimal.valueOf(12.6666), FormulaParserTest.calculate("6 * 2.1111")),
-                () -> assertEquals(BigDecimal.valueOf(1), FormulaParserTest.calculate("ROUNDCOST( 8 ) - 1")),
-                () -> assertEquals(BigDecimal.valueOf(-0.11), FormulaParserTest.calculate("- 1.22 + ROUNDOPERSUM( ROUNDCOST( 8.4444444 ) - 1 )")),
-                () -> assertEquals(BigDecimal.valueOf(3.67), FormulaParserTest.calculate("2 + CNTOP + CNTOP + ROUNDOPERSUM( RESERV + 1 )"))
+        Assertions.assertAll(                                                                                                   //чек
+                () -> assertEquals(0, BigDecimal.valueOf(2.11).compareTo(FormulaParserNewTest.calculate("2.22+CNTOP"))),
+                () -> assertEquals(0, BigDecimal.valueOf(-4).compareTo(FormulaParserNewTest.calculate("-6+2"))),
+                () -> assertEquals(0, BigDecimal.valueOf(12.6666).compareTo(FormulaParserNewTest.calculate("6*2.1111"))),
+
+                () -> assertEquals(0, BigDecimal.valueOf(-0.11).compareTo(FormulaParserNewTest.calculate("CNTOP"))),
+                () -> assertEquals(0, BigDecimal.valueOf(2.555).compareTo(FormulaParserNewTest.calculate("SUMOP"))),
+                () -> assertEquals(0, BigDecimal.valueOf(0.888888).compareTo(FormulaParserNewTest.calculate("RESERV"))),
+                () -> assertEquals(0, BigDecimal.valueOf(2322312.22).compareTo(FormulaParserNewTest.calculate("ROUNDOPERSUM(2322312.219999999"))),
+                () -> assertEquals(0, BigDecimal.valueOf(4.040404).compareTo(FormulaParserNewTest.calculate("ROUNDCOST(16.161616)"))),
+
+                () -> assertEquals(0, a1.compareTo(FormulaParserNewTest.calculate("ROUNDCOST(2133333123213.2342341341234)"))),
+                () -> assertEquals(0, a2.compareTo(FormulaParserNewTest.calculate("ROUNDOPERSUM(-13.234234564)"))),
+
+                () -> assertEquals(0, BigDecimal.valueOf(-3).compareTo(FormulaParserNewTest.calculate("-ROUNDCOST(8)-1"))),
+                () -> assertEquals(0, BigDecimal.valueOf(0).compareTo(FormulaParserNewTest.calculate("ROUNDCOST(0)"))),
+                () -> assertEquals(0, BigDecimal.valueOf(0).compareTo(FormulaParserNewTest.calculate("ROUNDOPERSUM(0)"))),
+                () -> assertEquals(0, BigDecimal.valueOf(2.33).compareTo(FormulaParserNewTest.calculate("1.22+ROUNDOPERSUM(ROUNDCOST(8.4444444)-1)"))),
+                () -> assertEquals(0, BigDecimal.valueOf(3.67).compareTo(FormulaParserNewTest.calculate("2+CNTOP+CNTOP+ROUNDOPERSUM(RESERV+1)")))
         );
 
 
-//        assertEquals(BigDecimal.valueOf(2.11), FormulaParserTest.calculate("2.22 + CNTOP"));
-//
-////        assertEquals(BigDecimal.valueOf(9), FormulaParserTest.calculate("2.8888 + 6.1112"));
-////        System.out.println("expected 9, actual: " + FormulaParserTest.calculate("2.8888 + 6.1112"));
-//
-//        assertEquals(BigDecimal.valueOf(-4), FormulaParserTest.calculate("- 6 + 2"));
-//        assertEquals(BigDecimal.valueOf(12.6666), FormulaParserTest.calculate("6 * 2.1111"));
-//        assertEquals(BigDecimal.valueOf(3), FormulaParserTest.calculate("6 / 2"));
-////        assertEquals(BigDecimal.valueOf(2), FormulaParserTest.calculate("SUMOP - 0.555"));
-////        assertEquals(BigDecimal.valueOf(1), FormulaParserTest.calculate("RESERV + 0.111112"));
-//        assertEquals(BigDecimal.valueOf(1), FormulaParserTest.calculate("ROUNDCOST( 8 ) - 1"));
-////        assertEquals(BigDecimal.valueOf(1), FormulaParserTest.calculate("ROUNDOPERSUM( 2.222222 ) - 1.22"));
-//        assertEquals(BigDecimal.valueOf(-0.11), FormulaParserTest.calculate("- 1.22 + ROUNDOPERSUM( ROUNDCOST( 8.4444444 ) - 1 )"));
-//        assertEquals(BigDecimal.valueOf(3.67), FormulaParserTest.calculate("2 + CNTOP + CNTOP + ROUNDOPERSUM( RESERV + 1 )"));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"23.23, 23.2344", "111111111111111.12, 111111111111111.123", "1111111111111111.11, 1111111111111111.11111233123","324.24, 324.2363333334234"})
+    void roundopersumCheck(String expected, String actual) {
+
+        BigDecimal a = new BigDecimal(expected, MathContext.DECIMAL32);
+        BigDecimal b = FormulaParserNewTest.FunctionType.ROUNDOPERSUM.apply(new BigDecimal(actual, MathContext.DECIMAL32));
+        assertEquals(0, a.compareTo(b));
     }
 }
